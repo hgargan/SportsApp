8a253dbc0733fa26910d6c8491171a683acaff1b
Henry Gargan <henrygargan@gmail.com>
2015-03-23 14:39:50 -0400

    First commit.

--- roster/admin.py
+++ roster/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from roster.models import Player
+from roster.models import Athlete, Event, Team
 
-admin.site.register(Player)
+class AthleteAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Athlete, AthleteAdmin)
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields= ('name',)
+    
+admin.site.register(Event, EventAdmin)
+admin.site.register(Team)
